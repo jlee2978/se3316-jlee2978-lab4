70f277dfdfe2826ee6d41507ce1f74cd1d19a099
Jeffrey <jlee2978@uwo.ca>
2019-11-06 17:36:15 -0500

    Generated Item and Login Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
