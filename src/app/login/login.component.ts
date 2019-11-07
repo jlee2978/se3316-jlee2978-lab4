2009d6a6b62dff02313951d3a634d7da0fa96661
Jeffrey <jlee2978@uwo.ca>
2019-11-06 22:18:13 -0500

    Added login capabilities

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  private userid: string;
+  private userpwd: string;
+  private loginError: boolean;
+  private role: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  login() {
+    this.role = '';
+
+    if (this.userid == 'admin' && this.userpwd == 'admin') {
+      this.role = 'admin'
+    }
+
+    if (this.userid == 'user' && this.userpwd == 'user') {
+      this.role = 'user'
+    }
+
+    // boolean loginError set to true if role has been assigned
+    // and false otherwise which indicates a login error
+    this.loginError = (this.role.length == 0);
+
+    if (!this.loginError) {
+      //if no error on login, navigate to /item/:userid/:role
+      this.router.navigate(['/item', this.userid, this.role]);
+    }
   }
 
 }
